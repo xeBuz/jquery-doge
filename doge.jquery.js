b8e591dcfe46a3543fb23e7fd0a9616f830679ee
Jesus Roldan <jesus.roldan@gmail.com>
2013-12-23 11:22:27 -0300

    Added Version and Manifest

--- doge.jquery.js
+++ doge.jquery.js
@@ -1,13 +1,3 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <jesus.roldan@gmail.com> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return Jesús Roldán
- * ----------------------------------------------------------------------------
- */
-
-
 (function($) {
 
     function DogePlugin(options) {
